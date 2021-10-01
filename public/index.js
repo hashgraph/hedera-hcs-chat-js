5dacbd02d2c377988fe043f2739700aa078ad97b
gregscullard <greg@scullard.com>
2021-10-01 15:41:51 +0200

    V2 sdk upgrade (#17)
    * Upgrades the SDK to v2, uses JSON for communication between server and client.
    
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>
    
    * Code reformat
    
    Signed-off-by: Greg Scullard <gregscullard@hedera.com>

--- public/index.js
+++ public/index.js
@@ -1,17 +1,12 @@
 $(function() {
   // expose our socket client
-  var socket = io();
-  
+  const socket = io();
+
   // handle and submit new chat messages to our server
   $("form").submit(function(e) {
     e.preventDefault(); // prevents page reloading
-    var msg = $("#m").val();
-    if(msg.includes("ℏ")) { 
-      alert("sorry, you're unable to send messages with ℏ in them!");
-    }
-    else { 
-      socket.emit("chat message", $("#m").val());
-    }
+    const msg = $("#m").val();
+    socket.emit("chat message", $("#m").val());
     $("#m").val("");
     return false;
   });
@@ -20,36 +15,34 @@ $(function() {
   // these are all sent over the Hedera consensus service!
   socket.on("chat message", function(msg) {
 
-    // Split this message by a specific, special character
-    var splitMsg = msg.split("ℏ");
-
+    const jsonMsg = JSON.parse(msg);
     // Grab the specifically formatted message string
-    var operatorId = splitMsg[0]; 
-    var clientId = splitMsg[1].slice(0,6) + "..";
-    var msg = splitMsg[2];
-    var sequenceNumber = splitMsg[3];
-    var trimmedHash = "runningHash: "+splitMsg[4].slice(0,6) + "..";
-    var trimmedTimestamp = splitMsg[5].slice(0,25);
-  
+    const operatorId = jsonMsg.operatorAccount;
+    const clientId = jsonMsg.client;
+    const theMessage = jsonMsg.message;
+    const sequenceNumber = jsonMsg.sequence;
+    const trimmedHash = "runningHash: " + jsonMsg.runningHash.slice(0,6) + "..";
+    const trimmedTimestamp = jsonMsg.timestamp.slice(0,25);
+
     // Grab & trim our topic ID
-    var topicId = document.getElementById("topic-id");
-    var idString = topicId.innerHTML.substring(7, topicId.length);
-  
-    // Append the split message to our HTML in pieces
+    const topicId = document.getElementById("topic-id");
+    const idString = topicId.innerHTML.substring(7, topicId.length);
+
+    // Append the message to our HTML in pieces
     $("#messages").append(
       $("<li>").addClass("new-message").append(
-        $("<div>").addClass("message").append( 
+        $("<div>").addClass("message").append(
           $("<p>").text(operatorId+"@"+clientId).addClass("client")).append(
-            $("<div>").addClass("message-body").append( 
-              $("<div>").text(msg).addClass("message-content")).append(
+            $("<div>").addClass("message-body").append(
+              $("<div>").text(theMessage).addClass("message-content")).append(
               $("<div>").text(trimmedTimestamp).addClass("message-timestamp")))).append(
-        $("<div>").addClass("meta").append( 
+        $("<div>").addClass("meta").append(
           $("<p>").text("sequence: "+ sequenceNumber).addClass("details")).append(
           $("<p>").text(trimmedHash).addClass("details")).append(
           $("<a>").text("view transaction").addClass("details")
             .attr("target", "_blank")
             .attr("href", `https://explorer.kabuto.sh/testnet/topic/${idString}/message/${sequenceNumber}`))));
-  
+
     // Update the current sequence #
     $("#sequence-number").text("last message sequence number: " + sequenceNumber + "  ");
   });
@@ -57,19 +50,19 @@ $(function() {
   // listen for new client connections from our server
   socket.on("connect message", function(msg) {
     /* send new connection message */
-    var splitMsg = msg.split("ℏ");
+    const connectMessage = JSON.parse(msg)
     $("#messages").append(
-      $("<li>").text('new connection: ' + splitMsg[0] + "@" + splitMsg[1]).addClass("new-connection"));
+      $("<li>").text('new connection: ' + connectMessage.operatorAccount + "@" + connectMessage.client).addClass("new-connection"));
     /* update this clients topic id */
-    var topicId = document.getElementById("topic-id");
-    topicId.innerHTML = "Topic: " + splitMsg[2];
+    const topicId = document.getElementById("topic-id");
+    topicId.innerHTML = "Topic: " + connectMessage.topicId;
   });
-  
+
   // listen for client disconnections from our server
   socket.on("disconnect message", function(msg) {
     /* send new disconnection message */
-    var splitMsg = msg.split("ℏ");
+    const disconnectMsg = JSON.parse(msg);
     $("#messages").append(
-      $("<li>").text(splitMsg[0] + "@" + splitMsg[1] + ' has disconnected').addClass("disconnection"));
+      $("<li>").text(disconnectMsg.operatorAccount + "@" + disconnectMsg.client + ' has disconnected').addClass("disconnection"));
   });
 });
