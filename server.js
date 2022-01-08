e748fd4231ac4b5ba13c17915ca670bf3a0c922f
Tammy Zeng <tammy.zeng908@gmail.com>
2022-01-08 15:48:43 -0500

    server.js: added mintBadge() and assignBadge()

--- server.js
+++ server.js
@@ -110,17 +110,17 @@ init(); // process arguments & handoff to runChat()
 function createBadge(name,symbol,max) {
     //Creating the NFT
     let badgeCreate = await new TokenCreateTransaction()
-    .setTokenName(name)
-    .setTokenSymbol(symbol)
-    .setTokenType(TokenType.NonFungibleUnique)
-    .setDecimals(0)
-    .setInitialSupply(0)
-    .setTreasuryAccountId(treasuryId) //maybe get treasury key from organizer?
-    .setMaxSupply(max)
-    .setSupplyKey(supplyKey) //check what the supply key should be
-    .freezeWith(hederaClient);
+        .setTokenName(name)
+        .setTokenSymbol(symbol)
+        .setTokenType(TokenType.NonFungibleUnique)
+        .setDecimals(0)
+        .setInitialSupply(0)
+        .setTreasuryAccountId(treasuryId) //maybe get treasury key from organizer?
+        .setMaxSupply(max)
+        .setSupplyKey(supplyKey) //check what the supply key should be
+        .freezeWith(hederaClient);
 
-    //Sign the transaction with the treasury key and submit to network
+    //Sign the transaction and submit to network
     let badgeCreateTxSign = await badgeCreate.sign(treasuryKey);
     let badgeCreateSubmit = await badgeCreateTxSign.execute(hederaClient);
 
@@ -128,9 +128,47 @@ function createBadge(name,symbol,max) {
     let badgeCreateRx = await badgeCreateSubmit.getReceipt(hederaClient);
     let tokenId = badgeCreateRx.tokenId;
     console.log(`- Created NFT with Token ID: ${tokenId} \n`);
+    return tokenId;
 }
 
-//Minting badges
+//Minting badge
+function mintBadge(CID, tokenId) {
+    let mintTx = await new TokenMintTransaction()
+        .setTokenId(tokenId)
+        .setMetadata([Buffer.from(CID)])
+        .freezeWith(hederaClient);
+    
+    //Sign the transaction and submit to network
+    let mintTxSign = await mintTx.sign(supplyKey);
+    let mintTxSubmit = await mintTxSign.execute(hederaClient);
+
+    //Get the transaction receipt information (serial number)
+    let mintRx = await mintTxSubmit.getReceipt(hederaClient);
+    console.log(`- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`);
+}
+
+//Assigning badge
+function assignBadge(txAccountId,txAccountKey,tokenId) {
+    //Associate new account with badge
+    let associateTx = await new TokenAssociateTransaction()
+		.setAccountId(txAccountId)
+		.setTokenIds([tokenId])
+		.freezeWith(hederaClient)
+		.sign(txAccountKey);
+
+    let associateTxSubmit = await associateTx.execute(hederaClient);
+    let associateRx = await associateTxSubmit.getReceipt(hederaClient);
+    console.log(`- NFT association with txAccount: ${associateRx.status}\n`);
+
+    //Transfer badge from treasury to txAccount
+	let tokenTransferTx = await new TransferTransaction()
+		.addNftTransfer(tokenId, 1, treasuryId, txAccountId)
+		.freezeWith(hederaClient)
+		.sign(treasuryKey);
+    let tokenTransferSubmit = await tokenTransferTx.execute(hederaClient);
+    let tokenTransferRx = await tokenTransferSubmit.getReceipt(hederaClient);
+    console.log(`\n- NFT transfer from Treasury to txAccount: ${tokenTransferRx.status} \n`);
+}
 
 /* have feedback, questions, etc.? please feel free to file an issue! */
 function sendHCSMessage(msg) {
