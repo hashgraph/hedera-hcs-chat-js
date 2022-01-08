eaedae39c5f9227e3575c2daef52820f6c0a7ada
Tammy Zeng <tammy.zeng908@gmail.com>
2022-01-08 16:16:53 -0500

    server.js: cleaned up and reorganized code

--- server.js
+++ server.js
@@ -29,88 +29,6 @@ const sleep = require("./utils.js").sleep;
 
 /* creating the NFTs */
 const supplyKey = PrivateKey.generate();
-const treasuryId = AccountId.fromString(process.env.TREASURY_ID); // maybe get treasury key from organizer??
-const treasuryKey = PrivateKey.fromString(process.env.TREASURY_PR_KEY);
-
-let operatorAccount = "";
-const hederaClient = Client.forTestnet();
-let topicId = "";
-let logStatus = "Default";
-
-/* configure our env based on prompted input */
-
-/* helper hedera functions */
-
-// Creating badge based on info supplied by the event organizer
-function createBadge(name,symbol,max) {
-    //Creating the NFT
-    let badgeCreate = await new TokenCreateTransaction()
-        .setTokenName(name)
-        .setTokenSymbol(symbol)
-        .setTokenType(TokenType.NonFungibleUnique)
-        .setDecimals(0)
-        .setInitialSupply(0)
-        .setTreasuryAccountId(treasuryId) //maybe get treasury key from organizer?
-        .setMaxSupply(max)
-        .setSupplyKey(supplyKey) //check what the supply key should be
-        .freezeWith(hederaClient);
-
-    //Sign the transaction and submit to network
-    let badgeCreateTxSign = await badgeCreate.sign(treasuryKey);
-    let badgeCreateSubmit = await badgeCreateTxSign.execute(hederaClient);
-
-     //Get the transaction receipt information
-    let badgeCreateRx = await badgeCreateSubmit.getReceipt(hederaClient);
-    let tokenId = badgeCreateRx.tokenId;
-    console.log(`- Created NFT with Token ID: ${tokenId} \n`);
-    return tokenId;
-}
-
-//Minting badge
-function mintBadge(CID, tokenId) {
-    let mintTx = await new TokenMintTransaction()
-        .setTokenId(tokenId)
-        .setMetadata([Buffer.from(CID)])
-        .freezeWith(hederaClient);
-    
-    //Sign the transaction and submit to network
-    let mintTxSign = await mintTx.sign(supplyKey);
-    let mintTxSubmit = await mintTxSign.execute(hederaClient);
-
-    //Get the transaction receipt information (serial number)
-    let mintRx = await mintTxSubmit.getReceipt(hederaClient);
-    console.log(`- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`);
-}
-
-//Assigning badge
-function assignBadge(txAccountId,txAccountKey,tokenId) {
-    //Associate new account with badge
-    let associateTx = await new TokenAssociateTransaction()
-		.setAccountId(txAccountId)
-		.setTokenIds([tokenId])
-		.freezeWith(hederaClient)
-		.sign(txAccountKey);
-
-    let associateTxSubmit = await associateTx.execute(hederaClient);
-    let associateRx = await associateTxSubmit.getReceipt(hederaClient);
-    console.log(`- NFT association with txAccount: ${associateRx.status}\n`);
-
-    //Transfer badge from treasury to txAccount
-	let tokenTransferTx = await new TransferTransaction()
-		.addNftTransfer(tokenId, 1, treasuryId, txAccountId)
-		.freezeWith(hederaClient)
-		.sign(treasuryKey);
-    let tokenTransferSubmit = await tokenTransferTx.execute(hederaClient);
-    let tokenTransferRx = await tokenTransferSubmit.getReceipt(hederaClient);
-    console.log(`\n- NFT transfer from Treasury to txAccount: ${tokenTransferRx.status} \n`);
-}
-
-//Get account balance
-function getAccountBalance(accountId, tokenId) {
-    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(accountId).execute(hederaClient);
-	console.log(`- Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
-}
-
 
 // Configure accounts and client, and generate needed keys
 const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
@@ -135,3 +53,73 @@ app.listen(port, () => {
 })
 
 module.exports = router;
+
+// Creating badge based on info supplied by the event organizer
+function createBadge(name,symbol,max) {
+    //Creating the NFT
+    let badgeCreate = await new TokenCreateTransaction()
+        .setTokenName(name)
+        .setTokenSymbol(symbol)
+        .setTokenType(TokenType.NonFungibleUnique)
+        .setDecimals(0)
+        .setInitialSupply(0)
+        .setTreasuryAccountId(treasuryId) //maybe get treasury key from organizer?
+        .setMaxSupply(max)
+        .setSupplyKey(supplyKey) //check what the supply key should be
+        .freezeWith(client);
+
+    //Sign the transaction and submit to network
+    let badgeCreateTxSign = await badgeCreate.sign(treasuryKey);
+    let badgeCreateSubmit = await badgeCreateTxSign.execute(client);
+
+     //Get the transaction receipt information
+    let badgeCreateRx = await badgeCreateSubmit.getReceipt(client);
+    let tokenId = badgeCreateRx.tokenId;
+    console.log(`- Created NFT with Token ID: ${tokenId} \n`);
+    return tokenId;
+}
+
+//Minting badge
+function mintBadge(CID, tokenId) {
+    let mintTx = await new TokenMintTransaction()
+        .setTokenId(tokenId)
+        .setMetadata([Buffer.from(CID)])
+        .freezeWith(client);
+    
+    //Sign the transaction and submit to network
+    let mintTxSign = await mintTx.sign(supplyKey);
+    let mintTxSubmit = await mintTxSign.execute(client);
+
+    //Get the transaction receipt information (serial number)
+    let mintRx = await mintTxSubmit.getReceipt(client);
+    console.log(`- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`);
+}
+
+//Assigning badge
+function assignBadge(txAccountId,txAccountKey,tokenId) {
+    //Associate new account with badge
+    let associateTx = await new TokenAssociateTransaction()
+		.setAccountId(txAccountId)
+		.setTokenIds([tokenId])
+		.freezeWith(client)
+		.sign(txAccountKey);
+
+    let associateTxSubmit = await associateTx.execute(client);
+    let associateRx = await associateTxSubmit.getReceipt(client);
+    console.log(`- NFT association with txAccount: ${associateRx.status}\n`);
+
+    //Transfer badge from treasury to txAccount
+	let tokenTransferTx = await new TransferTransaction()
+		.addNftTransfer(tokenId, 1, treasuryId, txAccountId)
+		.freezeWith(client)
+		.sign(treasuryKey);
+    let tokenTransferSubmit = await tokenTransferTx.execute(client);
+    let tokenTransferRx = await tokenTransferSubmit.getReceipt(client);
+    console.log(`\n- NFT transfer from Treasury to txAccount: ${tokenTransferRx.status} \n`);
+}
+
+//Get account balance
+function getAccountBalance(accountId, tokenId) {
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(accountId).execute(client);
+	console.log(`- Treasury balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+}
