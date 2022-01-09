5ea93574cfabd7e861743e25841cd2e017b22486
ivyd <iivy.dding@gmail.com>
2022-01-08 22:30:17 -0500

    start

---
+++ public/Router.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import React from "react";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const Router = () => (
+<BrowserRouter>
+    <Switch>
+        <Route path="/" component={}/>
+    </Switch>
+</BrowserRouter>
+);
+
+export default Router;
